b7330d34c81c6897e66be80ede5561bbc5349926
Jing Gao <jinggao@Jings-MacBook-Pro-2.local>
2017-05-04 14:28:35 +1000

    Complete Firebase storage download

--- social-network-clone/DataService.swift
+++ social-network-clone/DataService.swift
@@ -10,14 +10,19 @@ import Foundation
 import Firebase
 
 let DB_BAS = FIRDatabase.database().reference()
+let STORAGE_BASE = FIRStorage.storage().reference()
 
 class DataService {
     
     static let ds = DataService()
     
+    //DB references
     private var _REF_BASE = DB_BAS
     private var _REF_POSTS = DB_BAS.child("posts")
     private var _REF_USERS = DB_BAS.child("users")
+    
+    //Storage references
+    private var _REF_POST_IMAGES = STORAGE_BASE.child("post-pics")
     
     var REF_BASE: FIRDatabaseReference {
         return _REF_BASE
@@ -29,6 +34,10 @@ class DataService {
     
     var REF_USERS: FIRDatabaseReference {
         return _REF_USERS
+    }
+    
+    var REF_POST_IMAGES: FIRStorageReference {
+        return _REF_POST_IMAGES
     }
     
     func createFirbaseDBUser(uid: String, userData: Dictionary<String, String>) {
